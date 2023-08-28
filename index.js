84982fbb40a2b1531e193d27b8d78a958c5b0c56
imrob-s <orobsilva@gmail.com>
2023-08-28 12:03:33 -0300

    Criação do endpoint /oi

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+const express = require('express')
+const app = express()
+
+// Endpoint Principal
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+//Endpoint /oi
+app.get('/oi', function (req, res) {
+    res.send('Olá Mundo!')
+  });
+
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
