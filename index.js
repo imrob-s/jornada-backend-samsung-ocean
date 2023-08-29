1e14297dab6ed04e9a24004d9fe69e2b73664395
imrob-s <orobsilva@gmail.com>
2023-08-29 10:07:08 -0300

    Endpoint Read All implementado

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ app.get('/oi', function (req, res) {
     res.send('Olá Mundo!')
   });
 
+//Endpoints de herois
+const lista = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+//              0                   1                 2
+
+//Read All -> [GET] /herois
+app.get('/herois', function (req, res) {
+  res.send(lista);
+});
+
 app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
